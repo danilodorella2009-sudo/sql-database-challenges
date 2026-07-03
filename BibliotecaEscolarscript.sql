1aa81af4140633982129a0c42bfd3fd88b2caf3f
danilodorella2009-sudo <danilodorella2009@gmail.com>
2026-07-03 20:54:18 -0300

    Update BibliotecaEscolarscript.sql

--- BibliotecaEscolarscript.sql
+++ BibliotecaEscolarscript.sql
@@ -1,6 +1,12 @@
+-- ============================================================
+-- Script de criação do banco de dados 
+-- Sistema de Controle de Biblioteca Escolar
+-- ============================================================
+
 CREATE DATABASE IF NOT EXISTS sistemabibliotecaescolar;
 USE sistemabibliotecaescolar;
 
+-- Cadastro de cada aluno
 CREATE TABLE IF NOT EXISTS aluno (
     matricula INT PRIMARY KEY,
     nomeAluno VARCHAR(100) NOT NULL,
@@ -8,6 +14,7 @@ CREATE TABLE IF NOT EXISTS aluno (
     livrosEmprestados INT DEFAULT 0
 );
 
+-- Cadastro de cada livro
 CREATE TABLE IF NOT EXISTS livro (
     codigoLivro INT PRIMARY KEY,
     tituloLivro VARCHAR(100) NOT NULL,
